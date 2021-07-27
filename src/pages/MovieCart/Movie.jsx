905ec3eb5a0d039f4b47799ae3888e89bb5fee86
Ashok <67517969+Ashok-rio@users.noreply.github.com>
2021-07-27 10:40:56 +0530

    Update Movie.jsx

--- src/pages/MovieCart/Movie.jsx
+++ src/pages/MovieCart/Movie.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardBody, Col, Container, Row } from 'reactstrap';
+import { Col, Container, Row } from 'reactstrap';
 import API from '../../services/APIService';
 import {IMAGE_URL, IMDB, STAR } from '../../constants/Constants';
 
@@ -18,13 +18,13 @@ const Movie = (props) => {
           }
      };
 
-     React.useEffect(() => _getMovie(), []);
+     React.useEffect(() => _getMovie());
 
      return (
           <Container style={{ padding: '15px' }}>
                <Row>
                     <Col lg={5} md={6} sm={12} xs={12}>
-                         <img src={`${IMAGE_URL}${movie.poster_path}`} style={{ width: '80%', height: '100%' }} />
+                         <img src={`${IMAGE_URL}${movie.poster_path}`} style={{ width: '80%', height: '100%' }} alt={movie.original_title} />
                     </Col>
                     <Col lg={7} md={6} sm={12} xs={12}>
                          <p style={{ fontSize: '30px', fontWeight: 'bold' }}>{movie.original_title}</p>
@@ -39,7 +39,7 @@ const Movie = (props) => {
                                         justifyContent: 'center',
                                         alignItems: 'center',
                                    }}>
-                                   <img src={IMDB} style={{ width: '30px', height: '20px' }} />
+                                   <img src={IMDB} style={{ width: '30px', height: '20px' }} alt={'imdb'} />
                                    <p style={{ fontSize: '12px', fontWeight: 'bold', color: '#000', margin: '10px' }}>
                                         {movie.vote_average}
                                    </p>
@@ -50,7 +50,7 @@ const Movie = (props) => {
                                         justifyContent: 'center',
                                         alignItems: 'center',
                                    }}>
-                                   <img src={STAR} style={{ width: '20px', height: '20px' }} />
+                                   <img src={STAR} style={{ width: '20px', height: '20px' }} alt={'star'} />
                                    <p style={{ fontSize: '12px', fontWeight: 'bold', color: '#000', margin: '10px' }}>
                                         {movie.vote_count}
                                    </p>
